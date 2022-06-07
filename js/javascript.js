d66cc3c5c385b6b0b866fe08c9898a608a19de66
Ivan-Galvan <89554682+Ivan-Galvan@users.noreply.github.com>
2022-06-06 22:24:44 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+function darkmode() {
+  document.body.style.background = "black";
+  document.body.style.color = "white";
+
+}
+
+function copy() {
+  alert("Diseño web: Ivan Galvan | ivangalvan93@gmail.com")
+}
